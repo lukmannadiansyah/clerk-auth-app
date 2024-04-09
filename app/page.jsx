1e7ce55bf6bee4a2a6568c37ad88116ebae77ecf
lukmannadiansyah <lukmanadiansyah148@gmail.com>
2024-04-09 15:16:52 +0700

    Authentication with Clerk

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome</h1>
+      <p className="mb-5">This is a demo site Manscoo Next.js & Clerk. Go ahead and sign up or sign in!</p>
+    </>
+  );
+}
